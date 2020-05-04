e260aaf7a38bc7220118155b80932f90fc95b1a0
sleeping1943 <csleeping@163.com>
2020-05-04 23:28:54 +0800

    进行到导入包章节

---
+++ src/chapter02/mycolor/mycolor.go
@@ -0,0 +1,8 @@
+package mycolor
+
+const (
+	White = 0
+	Black = 1
+)
+
+func foo() int { return White }
---
+++ src/chapter02/test_import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	
+	"chapter02/mycolor"
+)
+
+func main() {
+	fmt.Printf("black is %d", mycolor.Black)
+}
